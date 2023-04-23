be8046ee4bd392ed67d67e0fad83cef9158602df
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-04-23 13:48:11 +0100

    [GoogleDevTrainig] - created GreetingCard app

---
+++ GreetingCard/app/src/main/java/com/patriciamolnar/greetingcard/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.patriciamolnar.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.patriciamolnar.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hi, my name is $name!",
+        modifier = Modifier
+            .border(BorderStroke(1.dp, Color.Magenta))
+            .padding(24.dp))
+//    Text(text = "Hi, my name is $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GreetingCardTheme {
+        Surface(color = Color.Cyan) {
+            Greeting("燕芷")
+        }
+    }
+}
