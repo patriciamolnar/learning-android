92f1a99d90ddd404ed2690f0e7f06cb04cdc361e
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-04-30 09:26:36 +0100

    [birthdayGreeting] - displaying text elements in a row

--- HappyBirthday/app/src/main/java/com/patriciamolnar/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/patriciamolnar/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.patriciamolnar.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.patriciamolnar.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,16 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message
-    )
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Row {
+        Text(
+            text = message,
+            fontSize = 30.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
 }
 
 @Preview(showBackground = true, name = "GreetingCardPreview", showSystemUi = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("HAPPY BIRTHDAY ANDROID!")
+        BirthdayGreetingWithText("生日快乐，莫先生！", "- 成燕芷")
     }
 }
