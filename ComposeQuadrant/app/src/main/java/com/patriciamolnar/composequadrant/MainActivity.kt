c3233e8df30e6ab2f02d690e2ba32037354abb3f
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-05-01 14:55:58 +0100

    Compose Quadrant exercise pt.1

---
+++ ComposeQuadrant/app/src/main/java/com/patriciamolnar/composequadrant/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.patriciamolnar.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.patriciamolnar.composequadrant.ui.theme.ComposeQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeQuadrantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateLayout() {
+    Row() {
+        for (i in 1..4) {
+            AddTile(title = i.toString())
+        }
+    }
+}
+
+@Composable
+fun AddTile(title: String) {
+    Text(text = title)
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ComposeQuadrantTheme {
+        CreateLayout()
+    }
+}
