79e837c53fec6b6182f8b3e8d4c9811db7cdf339
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-05-01 16:36:22 +0100

    compose quadrant pt. 2

--- ComposeQuadrant/app/src/main/java/com/patriciamolnar/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/patriciamolnar/composequadrant/MainActivity.kt
@@ -3,14 +3,21 @@ package com.patriciamolnar.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.patriciamolnar.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,22 +38,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateLayout() {
-    Row() {
-        for (i in 1..4) {
-            AddTile(title = i.toString())
+fun CreateLayout(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.border(2.dp, Color.Cyan)) {
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = modifier
+                    .background(color = Color.Green, shape = RectangleShape)
+                    .weight(1F)
+            ) {
+                Text(text = "Text composable")
+                Text(text = "Displays text and follows Material Design guidelines.\n")
+            }
+
+            Column(
+                modifier = modifier
+                    .background(color = Color.Yellow, shape = RectangleShape)
+                    .weight(1F)
+            ) {
+                Text(text = "Image composable\n")
+                Text(text = "Creates a composable that lays out and draws a given Painter class object.\"")
+            }
+        }
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            CreateTile(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                color = Color.Cyan,
+                modifier = modifier.weight(1F)
+            )
+            CreateTile(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                color = Color.LightGray,
+                modifier = modifier.weight(1F)
+            )
         }
     }
 }
 
 @Composable
-fun AddTile(title: String) {
-    Text(text = title)
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateTile(
+    title: String,
+    description: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.background(color = color, shape = RectangleShape)
+    ) {
+        Text(text = title)
+        Text(text = description)
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
