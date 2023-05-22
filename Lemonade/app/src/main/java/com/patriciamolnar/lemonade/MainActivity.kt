7802aa1de56503d0c3be38f591f37b90c1b06493
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-05-22 07:54:11 +0100

    [lemonade] - adding header design

--- Lemonade/app/src/main/java/com/patriciamolnar/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/patriciamolnar/lemonade/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -25,8 +24,10 @@ import androidx.compose.ui.unit.dp
 import com.patriciamolnar.lemonade.ui.theme.LemonadeTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,13 +57,33 @@ fun DefaultPreview() {
 
 @Composable
 fun LemonadeHeader(modifier: Modifier) {
-    Row(modifier = modifier
+    val lemonadeIcon = painterResource(id = R.drawable.iconmonstr_lemonade)
+    val headerPadding = 12.dp
+
+    Column(modifier = modifier
         .fillMaxWidth()
         .background(color = Color(0xFF102119), RectangleShape)) {
+        Row(modifier = modifier.padding(headerPadding), horizontalArrangement = Arrangement.Center) {
+            Text(
+                color = Color.Yellow,
+                fontSize = 30.sp,
+                text = stringResource(R.string.header_title)
+            )
+            Icon(
+                painter = lemonadeIcon,
+                contentDescription = null,
+                tint = Color.Yellow,
+                modifier = modifier.size(35.dp)
+            )
+        }
         Text(
             color = Color(0xFFBDDBB9),
-            text = "Cats making Lemonade (not for you though)",
-            modifier = Modifier.padding(12.dp)
+            text = stringResource(R.string.header_subtitle),
+            textAlign = TextAlign.End,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.padding(
+                bottom = headerPadding, start = headerPadding, end = headerPadding
+            ).fillMaxWidth()
         )
     }
 }
@@ -89,7 +110,9 @@ fun CatsMakingLemonadeApp() {
 
     Column(modifier = Modifier.background(color = Color(0xFF34614C))) {
         LemonadeHeader(modifier = Modifier)
-        Column(modifier = Modifier.fillMaxSize().padding(10.dp),
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
--- Lemonade/app/src/main/java/com/patriciamolnar/lemonade/ui/theme/Type.kt
+++ Lemonade/app/src/main/java/com/patriciamolnar/lemonade/ui/theme/Type.kt
@@ -2,12 +2,24 @@ package com.patriciamolnar.lemonade.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.patriciamolnar.lemonade.R
 
+val Outfit = FontFamily(
+    Font(R.font.outfit_regular),
+    Font(R.font.outfit_semibold, FontWeight.SemiBold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = Outfit,
+    h1 = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
